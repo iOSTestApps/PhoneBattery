4680c4b0831307af0e0b3c1cc574aa186e3e9c6d
Marcel Voss <marcel.voss@me.com>
2015-06-19 14:37:19 +0200

    First changes like adding the Watch extension and other required files.

--- PhoneBattery/AppDelegate.swift
+++ PhoneBattery/AppDelegate.swift
@@ -17,6 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
         // Override point for customization after application launch.
+        
+        self.window?.rootViewController = MainViewController()
+        
         self.window!.backgroundColor = UIColor.whiteColor()
         self.window!.makeKeyAndVisible()
         return true
---
+++ PhoneBattery/MainViewController.swift
@@ -0,0 +1,26 @@
+//
+//  MainViewController.swift
+//  PhoneBattery
+//
+//  Created by Marcel Voss on 19.06.15.
+//  Copyright (c) 2015 Marcel Voss. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}

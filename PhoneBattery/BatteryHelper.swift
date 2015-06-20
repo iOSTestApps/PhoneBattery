042df896b9901c2b7e5daa5a8a5c95c1a2b40c74
Marcel Voss <marcel.voss@me.com>
2015-06-20 11:17:15 +0200

    Added the basic functionalty. The only that's missing is a proper UI.

--- PhoneBattery/BatteryHelper.swift
+++ PhoneBattery/BatteryHelper.swift
@@ -1,33 +1,20 @@
 //
-//  InterfaceController.swift
-//  PhoneBattery WatchKit Extension
+//  BatteryHelper.swift
+//  PhoneBattery
 //
-//  Created by Marcel Voss on 19.06.15.
+//  Created by Marcel Voss on 20.06.15.
 //  Copyright (c) 2015 Marcel Voss. All rights reserved.
 //
 
-import WatchKit
-import Foundation
+import UIKit
 
-
-class InterfaceController: WKInterfaceController {
+class BatteryHelper: NSObject {
     
     let device = UIDevice.currentDevice()
     var batteryLevel : Float?
     var batteryState : UIDeviceBatteryState!
-
-    @IBOutlet weak var statusLabel: WKInterfaceLabel!
     
-    override func awakeWithContext(context: AnyObject?) {
-        super.awakeWithContext(context)
-        
-        // Configure interface objects here.
-    }
-
-    override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
-        super.willActivate()
-        
+    func setup() {
         device.batteryMonitoringEnabled = true
         batteryLevel = device.batteryLevel
         batteryState = device.batteryState
@@ -35,22 +22,13 @@ class InterfaceController: WKInterfaceController {
         // KVO for oberserving battery level and state
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "batteryLevelChanged:", name: UIDeviceBatteryLevelDidChangeNotification, object: device)
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "batteryStateChanged:", name: UIDeviceBatteryStateDidChangeNotification, object: device)
-
-        statusLabel.setText(String(format: "%.f%%", batteryLevel! * 100))
-    }
-
-    override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
-        super.didDeactivate()
     }
     
-    func batteryLevelChanged(notification: NSNotification) {
+    func batteryLevelChanged() {
         batteryLevel = device.batteryLevel
-        
-        statusLabel.setText(String(format: "%.f%%", batteryLevel! * 100))
     }
     
-    func batteryStateChanged(notification: NSNotification) {
+    func batteryStateChanged() {
         batteryState = device.batteryState
         
         if batteryState == UIDeviceBatteryState.Full {
@@ -63,7 +41,6 @@ class InterfaceController: WKInterfaceController {
             // State is unknown
             
         }
-        
     }
-
+    
 }
--- PhoneBattery/MainViewController.swift
+++ PhoneBattery/MainViewController.swift
@@ -9,11 +9,57 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    
+    let device = UIDevice.currentDevice()
+    var batteryLevel : Float?
+    var batteryState : UIDeviceBatteryState!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        device.batteryMonitoringEnabled = true
+        batteryLevel = device.batteryLevel
+        batteryState = device.batteryState
+        
+        // KVO for oberserving battery level and state
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "batteryLevelChanged:", name: UIDeviceBatteryLevelDidChangeNotification, object: device)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "batteryStateChanged:", name: UIDeviceBatteryStateDidChangeNotification, object: device)
+        
+        
+        let percentageLabel = UILabel()
+        percentageLabel.textAlignment = .Center
+        percentageLabel.text = String(format: "%.f%%", batteryLevel! * 100)
+        percentageLabel.font = UIFont.systemFontOfSize(20)
+        percentageLabel.setTranslatesAutoresizingMaskIntoConstraints(false)
+        self.view.addSubview(percentageLabel)
+        
+        self.view.addConstraint(NSLayoutConstraint(item: percentageLabel, attribute: NSLayoutAttribute.CenterX, relatedBy: NSLayoutRelation.Equal, toItem: self.view, attribute: NSLayoutAttribute.CenterX, multiplier: 1.0, constant: 0))
+        
+        self.view.addConstraint(NSLayoutConstraint(item: percentageLabel, attribute: NSLayoutAttribute.CenterY, relatedBy: NSLayoutRelation.Equal, toItem: self.view, attribute: NSLayoutAttribute.CenterY, multiplier: 1.0, constant: 0))
+        
+        
+        
+        
+        
+        /*
+        let aboutButton = UIButton.buttonWithType(UIButtonType.System) as! UIButton
+        aboutButton.setTitle("", forState: UIControlState.Normal)
+        aboutButton.layer.cornerRadius = 20
+        aboutButton.backgroundColor = UIColor.blackColor()
+        aboutButton.setTranslatesAutoresizingMaskIntoConstraints(false)
+        self.view .addSubview(aboutButton)
+        
+        self.view.addConstraint(NSLayoutConstraint(item: aboutButton, attribute: .Bottom, relatedBy: .Equal, toItem: self.view, attribute: .Bottom, multiplier: 1.0, constant: -30))
+        
+        self.view.addConstraint(NSLayoutConstraint(item: aboutButton, attribute: .Width, relatedBy: .Equal, toItem: self.view, attribute: .Width, multiplier: 1.0, constant: -100))
+        
+        self.view.addConstraint(NSLayoutConstraint(item: aboutButton, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .Height, multiplier: 1.0, constant: 50))
+        
+        self.view.addConstraint(NSLayoutConstraint(item: aboutButton, attribute: .CenterX, relatedBy: .Equal, toItem: self.view, attribute: .CenterX, multiplier: 1.0, constant: 0))*/
+        
+        
+        
+        //percentageLabel.addConstraint(NSLayoutConstraint(ite)
         
         
     }
@@ -21,6 +67,26 @@ class MainViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func batteryLevelChanged(notification: NSNotification) {
+        batteryLevel = device.batteryLevel
+    }
+    
+    func batteryStateChanged(notification: NSNotification) {
+        batteryState = device.batteryState
+        
+        if batteryState == UIDeviceBatteryState.Full {
+            
+        } else if batteryState == UIDeviceBatteryState.Charging {
+            
+        } else if batteryState == UIDeviceBatteryState.Unplugged {
+            
+        } else {
+            // State is unknown
+            
+        }
+        
     }
     
 }
